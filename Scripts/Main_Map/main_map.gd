a76d13d062ac8363a1809b93d856b3d89243ffe6
Diviyan20 <diviyanr@gmail.com>
2025-05-24 11:16:54 +0800

    Ground Parallax

--- Scripts/Main_Map/main_map.gd
+++ Scripts/Main_Map/main_map.gd
@@ -4,10 +4,12 @@ const PLAYER_START_POS: = Vector2i(288,936)
 const CAM_START_POS: = Vector2i(960, 536)
 
 var speed : float
+var screen_size: Vector2i
 const START_SPEED: float = 10.0
 const MAX_SPEED: int = 25
 
 func _ready():
+	screen_size = get_window().size
 	new_game()
 	pass
 	
@@ -17,8 +19,13 @@ func new_game():
 	$Camera2D.position = CAM_START_POS
 	pass
 
-func _process(delta):
+func _process(_delta):
 	speed = START_SPEED
 	$Player.position.x += speed
 	$Camera2D.position.x += speed
+	
+	#update ground position
+	if $Camera2D.position.x - $Ground.position.x > screen_size.x * 1.5:
+		$Ground.position.x +=screen_size.x
+		$Ceiling.position.x +=screen_size.x
 	pass
