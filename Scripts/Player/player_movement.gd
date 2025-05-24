5389133c4dc1d77d2239cc6f90b0195d9663d2e7
Diviyan20 <diviyanr@gmail.com>
2025-05-24 10:41:32 +0800

    Teleport Functionality

---
+++ Scripts/Player/player_movement.gd
@@ -0,0 +1,22 @@
+extends CharacterBody2D
+
+@onready var sprite = $AnimatedSprite2D
+var on_ceiling = false
+
+# Set these to match your game’s actual floor and ceiling Y positions
+const FLOOR_Y = 936
+const CEILING_Y = 305
+
+func _input(event):
+	if event.is_action_pressed("ui_accept"):  # Space key by default
+		teleport()
+
+func teleport():
+	if on_ceiling:
+		global_position.y = FLOOR_Y
+		sprite.flip_v = false
+	else:
+		global_position.y = CEILING_Y
+		sprite.flip_v = true
+
+	on_ceiling = !on_ceiling
