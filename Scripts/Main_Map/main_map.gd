b0d1d613094152ec418ff27cd47b53bf97a933c1
Diviyan20 <diviyanr@gmail.com>
2025-05-24 11:31:23 +0800

    Score System

--- Scripts/Main_Map/main_map.gd
+++ Scripts/Main_Map/main_map.gd
@@ -4,6 +4,7 @@ const PLAYER_START_POS: = Vector2i(288,936)
 const CAM_START_POS: = Vector2i(960, 536)
 
 var speed : float
+var score: int
 var screen_size: Vector2i
 const START_SPEED: float = 10.0
 const MAX_SPEED: int = 25
@@ -14,6 +15,8 @@ func _ready():
 	pass
 	
 func new_game():
+	#reset all variables
+	score = 0
 	$Player.position = PLAYER_START_POS
 	$Player.velocity = Vector2i(0,0)
 	$Camera2D.position = CAM_START_POS
@@ -21,6 +24,7 @@ func new_game():
 
 func _process(_delta):
 	speed = START_SPEED
+	show_score()
 	$Player.position.x += speed
 	$Camera2D.position.x += speed
 	
@@ -28,4 +32,14 @@ func _process(_delta):
 	if $Camera2D.position.x - $Ground.position.x > screen_size.x * 1.5:
 		$Ground.position.x +=screen_size.x
 		$Ceiling.position.x +=screen_size.x
+	pass
+
+func show_score():
+	$HUD.get_node("Score Label").text = "SCORE: " + str(score)
+	pass
+
+
+func _on_score_timer_timeout() -> void:
+	score += 1
+	show_score()
 	pass
